2957be453137469bc357ce34a67250bc774736b6
[Centro1993] <[jonas.leitner93@gmail.com]>
2016-05-16 17:31:44 +0200

    updated backend

--- events.js
+++ events.js
@@ -18,125 +18,176 @@ const port = 3001;
 
 /*------------------------NEDB-FUNKTIONEN----------------------*/
 //event in db einfügen
-var dbCreateEvent = function (event, callback) {
-  db.events.insert(doc, function (err, res) {
+var dbCreateEvent = function(event, callback) {
+  db.events.insert(doc, function(err, res) {
     callback(err, res);
   });
 }
 
 //alle events finden (um sie auf der website anzuzeigen)
 //alternativ nach einer eventid suchen
-var dbQueryEvents = function (eventid, callback) {
-  if(event == null) {
-  db.events.find({}, function (err, docs) {
-   callback(err, docs);
- });
- } else {
-   db.events.find({_id: eventid}, function (err, docs) {
-    callback(err, docs);
-  });
- }
+var dbQueryEvents = function(eventid, callback) {
+  if (event == null) {
+    db.events.find({}, function(err, docs) {
+      callback(err, docs);
+    });
+  } else {
+    db.events.find({
+      _id: eventid
+    }, function(err, docs) {
+      callback(err, docs);
+    });
+  }
 }
 
 //user in usercollection einfügen
 var dbInsertUser = function(user, eventId, callback) {
-  db.users.insert(user, function(err ,res) {
+  db.users.insert(user, function(err, res) {
     callback(err, res, eventId);
   });
 }
 
 //checken, ob im event noch platz ist, und emails schicken
 var dbProcessSignup = function(user, event, callback) {
+  //DEFINITION PARTICIPANT
+  var participant = {
+    user['_id'],
+    verified: false,
+    timestamp: new Date()
+  }
+
+  //user in event eintragen als unbestätigt eintragen
+  event.participants.push(participant);
   //nach freien plätzen prüfen
-  if(event)
+  if (eventCheckPlaces(event) > 0) {
+    //email mit bestätigungsanfrage schicken
+
+  } else {
+    //wartelistenmail schicken
+
+  }
+  //event in db updaten
+  db.events.update({event['_id']}, event, {upsert: true}, function(err ,res) {
+    callback(err, res);
+  });
+
 }
 
+/*---------------------------FUNKTIONEN----------------*/
+
+//TODO event nach anzahl freier plätze überprüfen
+var eventCheckPlaces = function(event) {
+
+  return places;
+}
+
+eventVerifyUser()
 
 /*----------------------HTTP-DISPATCHER------------------------*/
 var handleRequest = function(req, res) {
-  {
-    //dispatcher einrichten
-    try {
-      console.log(req.url);
-      dispatcher.dispatch(req, res);
-    } catch (err) {
-      console.log(err);
+{
+  //dispatcher einrichten
+  try {
+    console.log(req.url);
+    dispatcher.dispatch(req, res);
+  } catch (err) {
+    console.log(err);
+  }
+  //relativen ressourcenpfad setzen
+  dispatcher.setStatic('resources');
+  dispatcher.setStaticDirname('/');
+
+  //alle events anfordern
+  dispatcher.onGet('/queryEvents', function(req, res) {
+    dbQueryEvents(function(err, events) {
+        if (err == null) {
+          //bestätigung senden
+          res.writeHead(200, {
+            'Content-type': 'text/HTML'
+          });
+          res.end(JSON.stringify(events));
+        });
     }
-    //relativen ressourcenpfad setzen
-    dispatcher.setStatic('resources');
-    dispatcher.setStaticDirname('/');
-
-    //alle events anfordern
-    dispatcher.onGet('queryEvents', function(req, res) {
-      dbQueryEvents(function(err, events) {
-        if(err == null) {
-        //bestätigung senden
-        res.writeHead(200, {
-          'Content-type': 'text/HTML'
-        });
-        res.end(JSON.stringify(events));
-      });} else {
-        //error senden
-        res.writeHead(404, {
-          'Content-type': 'text/HTML'
-        });
-        res.end(err);
-      }
+    else {
+      //error senden
+      res.writeHead(404, {
+        'Content-type': 'text/HTML'
       });
-    });
+      res.end(err);
+    }
+  });
+});
 
-    dispatcher.onPost("/signup", function(req, res) {
-      var body = JSON.parse(req.body);
+//email-link, welcher den user auf verifiziert setzt
+dispatcher.onGet('/verifySignup', function(req, res) {
+  var body = JSON.parse(req.body);
 
-      var eventId = body.eventId;
+  userId = body.userid;
+  eventId = body.eventid;
+//event suchen
+//freie plätze überprüfen
+//user in event eintragen oder nicht, email schicken
+  async.waterfall([
+    async.apply(dbQueryEvents, eventId),
+    eventCheckPlaces
+  ], function(err, places) {
+    if(places )
+  })
+});
 
-      //definition user
-      var user = {
-        email = body.email,
-        name = body.name,
-        verified = false,
-        timestamp = new Date();
-      }
+dispatcher.onPost("/signup", function(req, res) {
+  var body = JSON.parse(req.body);
 
-      async.waterfall([
-        async.apply(dbInsertUser, user, eventId),
-        dbQueryEvent,
-        dbProcessSignup
-      ], function(err, res) {
-        //rückmeldung an user
-      });
+  var eventId = body.eventId;
+
+  //definition user
+  var user = {
+    email = body.email,
+    name = body.name,
+    verified = false
+  }
+
+  async.waterfall([
+    async.apply(dbInsertUser, user, eventId),
+    dbQueryEvent,
+    dbProcessSignup
+  ], function(err, res) {
+
+  });
 
 
-    });
+});
 
-    dispatcher.onPost("/createEvent", function(req ,res) {
-      var body = JSON.parse(req.body);
+dispatcher.onPost("/createEvent", function(req, res) {
+  var body = JSON.parse(req.body);
 
-      //definition event
-      var event = {
-        name: body.name,
-        creator: body.creator,
-        description: body.description,
-        time: body. time,
-        maxParticipants: body. maxParticipants,
-        participants: [],
-      }
+  //definition event
+  var event = {
+    name: body.name,
+    creator: body.creator,
+    description: body.description,
+    time: body.time,
+    maxParticipants: body.maxParticipants,
+    participants: [],
+  }
 
-      dbCreateEvent(event, function(err, res) {
-        if(err == null) {
+  dbCreateEvent(event, function(err, res) {
+      if (err == null) {
         //bestätigung senden
         res.writeHead(200, {
           'Content-type': 'text/HTML'
         });
         res.end();
-      });} else {
-        //error senden
-        res.writeHead(404, {
-          'Content-type': 'text/HTML'
-        });
-        res.end(err);
-      }
+      });
+  }
+  else {
+    //error senden
+    res.writeHead(404, {
+      'Content-type': 'text/HTML'
     });
+    res.end(err);
+  }
+});
 }
 
 //server erstellen
